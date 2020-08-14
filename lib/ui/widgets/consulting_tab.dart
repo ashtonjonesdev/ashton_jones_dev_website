ffa1264f002b2c3e0efa7cfc13dba440a3abbc6b
Ashton Jones <ashtonjonesdev@gmail.com>
2020-08-14 16:31:29 -0400

    Tried using service account with GSuite

--- lib/ui/widgets/consulting_tab.dart
+++ lib/ui/widgets/consulting_tab.dart
@@ -27,17 +27,17 @@ class _ConsultingTabState extends State<ConsultingTab> {
   final _credentials = new ServiceAccountCredentials.fromJson(r'''
 {
   "type": "service_account",
-  "project_id": "ashton-jones-dev-website",
-  "private_key_id": "9cf385141ecab9b43bb35f50d9b910a5c8fc094a",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCQjA4DCVyDCa/h\nedxGwESWRuJxXPxnyB/IqokpD7CCIosGLzyiVzcMHo8QpoT72fgk5/fxiZsgG0g9\nMC3MI54tpl8pYtmp1ubrzdtgcGkF1xy7nOl2nb75AYaKP4QpPfDY2QV6lQGzsGKT\nYw7VuLXxu7OQYb7le1zTFslfswFmSbPQoqeun2iGBlh2QDZPanQ2hcn34LC5zuyi\n8cITXf1Pt/AHu2jtaYrHLcalyS3QV9HlHF3xw2SP2MOvh1Om/jFgipjLmTXW2LTb\nMUk3RExB+S5XN6uuA3Yzo86lt/rvfdxeaKuyo+AcNoXE4h20GRnU4iEEAXvvfguG\nCa30j8wnAgMBAAECggEALokYfbvGz90PhazA0PpXWnYpsst360WPjf/xNon5jnvm\nfDA6+hzP+RPhhT/G8nojHGad+WIO0hBWenSHWgBhjVrRqjZL0xT/tQzKQssN1LaD\nZpdGxkxUzj/Ce8GlH830OyvrSqKsMJhps/+hKAdWdCO01JfTaHgF02Ozox6zJaW6\n0aq4o8obKZGoGfNSVv2090AXNypdDhy/mTqseZhYvk++AcRWCCO8us0NhzdZpBjh\nuDetQ0VE58AnlsK8AN4tVhwZJnWVeesVnmp/u8/GX/KD1Co3lfDGOfdkmeKcoU+j\nsf+KwdUlw7D0BSF4rOxyHjK/eEJOCv1HCpXWlkc+qQKBgQDHJQe2NsYqvvOoY1Db\nADM4b5T0rx5pf9up3tPR04Z0RWKcwidARGLzDdYgT9XeucWmVtFwa4rcfhTe6nPu\nbFB3QkhgdEL3wGtLScFjflk0U7QYf6HkWxgpkOhv/zkcMI/Rzl4bYNr9Obwb5rWq\nPijdKp4KrvPZB2uUrUYK6v1yHQKBgQC50KEjvgS3Jpad7WbRQNM7R7kAlbN8Lq5J\nYQVrs8PytpVilwZ4B8b92vp8iIkYkWPJbs1RfbJiRiTSf4YeUKZY9qy18CWHOI0u\n0ylgfsT+I4Yu3WV3iJemiMV1LbGwrTx7mcTvEE11lw+oBo8xqDq4/8VSonEpdgPz\n1VHfiFFkEwKBgDXFqe9pmtiv9trg9lQsGaiDeJUZM/0IqSeVI1zk4MlEcvwg8LhJ\nvu4iLk9LP1SElPn1N1hDsZsX8ErAw8z9Mj0BiMmCmxKKwm3x3GYhF7UpZNe4nt4q\nOrv+hMS5BZK3Ch58uqxp2rfFfyWJhkQRxLbzavk3hGB1wHbE0sTf7s+BAoGBAJwF\nn1h4tFLpNuzyqzOnP/Z1udTQ3n22YKfCrJ+LuUuynerDry3YMfF0DoHEz4Dui6BW\nxlXXupIkHcUWSRJpW9Q1hD3coFZTWNDpg8O/jj9qqr/tLMbazaOLEmna7WnRfIi4\nTBEEWlYTepT2tQPYIz61nQVO2sAalSUHiJCuUNE7AoGACeBN6JS1fBCjyLhT/GNY\nxz/lyvZcAec4nscb3COZfylSVKvb3K8QFuD3xr+Y3QuMgjyVloATmTBadk+qn0a0\nGDjRwoRA0mzX8a+1PnX7CU7FEzJGhIqUQADxDVOfs+CutvGi58vgy2PDfC6DvnCp\nFlbNo4OQ+8qbHfoMMJsV01A=\n-----END PRIVATE KEY-----\n",
-  "client_email": "oauth-service-ashton-jones-dev@ashton-jones-dev-website.iam.gserviceaccount.com",
-  "client_id": "108668530476668501164",
+  "project_id": "midyear-pattern-286419",
+  "private_key_id": "4c513d1d895aa754e0876faf571ecd3fd4493698",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCsGL9vd9GNUzg9\nB2Ni1bmERtDAOxeYEzcXstjPyj34uCxzDy37M/kQQe1WHqCXx3BzDlLGOTdPCBVy\nPMZPhk/WxlLXhscealctvzB0dzvZKVUVLzWOhACooRJV19W4cjVC0+1ICnysLeJP\nQmjrnjPmWf7XOiFSBzG4zNF2j2Fh1HD8tRG3171mhON/cuCkw+F+23DclqqRCEo+\nAk9/UfajguKw5GDyCOpDnYp5obJb/6VzZY9OrNhjcktViLuTqOUBdG3z9o6Pyhz7\no3kjSdO1ycqABE+efu2gcSo2okdOlRT81AZTwag+IDrjErxjOGZoXZhwn4IMlnhU\nE+LGX3CPAgMBAAECggEAGGNWwwMl18iSpa4QWtySx7i0CH8snskfpjoaw4eTigBU\n5uagdi2CqYJ45xq8A1uosdq2cY+A07BU6bV3HPnvAoQPD//yOSXfaNo5PFKmppBW\n6wOfrAHviKDFf74bHrerQzx+hGuys0EQBc9myE0eabxt2C5NXoR/0AJ72eNZF4sk\nrNWBJ4lz7/K81JZzMfQjOFW0cI8tJKPtJXsVyXIIn/w0jIarkcebxXE7SGkHUTl6\n3vfNNcRO1VhCKDRyauXq+m2g7bRDug2Yy3wVkYVV8oWMpqVCI/zq0cMAHqfd4Pd8\nhxnCTCSML6K8ZFlMmD8dpzrzlx8rb72nth/CCQvXcQKBgQDZ9ZHh8OfiqiXj5+y8\nzRY29/vR3i2/qgFuNbDRsJSx7DUS9ZBLQ4Fxkgfo6qdsTkid/ZaHUA0SZehIUjOM\nkMvwK6vtb6nZ08GrhB0vMLwMlTCOtE5xhaHoN0QF9oxUXLO1dTE/3ktgCkdgg4PJ\nKRzcPDlyDjYGYtYeuxeGaX8AiwKBgQDKIgkOTbXReOlZsOcw7wQq7WM/VoQthYLZ\npDwVumIjdFYVnW7yvG0KVWGkmbgbsHOkA4bFUalUtc4EizoijT7jEvWiLqIdw7ug\nGC67zKfGzO1F0uXmXmzZMnmggHoC8ixq3sStfPkX/xmg75TzlrH9N3/cmgQfWjBQ\nO1YklkTsjQKBgQCuiPdI9qC54YZ1+kKY7aHcYrhEVqZL1NdQaa4uPDjWQVA+3mWt\n32DIkSfEXCO1lvqPiYIA2Rmeofxf7XNaHJkyucXzXHem9usfbavxHhV3/f/kRn6K\naXg9fAP8wnGdHJ7LsAzAGEJDpfeCUc42R7XUpmVZ8AdTFdJgyXENMW2CAQKBgQC4\n/NEg4AnH/UpCJV5QME62Iq8z5i/toUSRfnbmw77RB8VI3t4NFFkOnflQtMYDwUb0\nEa4jo4q+1ewHyIuZ1354Pn/Cv77qUpnB+J+8i534mM+FmW+Bl2UymbkqJKqFS7FO\n1TVWOQuW5IZF/T1qeL3IKiTAdcvUDawtoUBDF6tYeQKBgEqA8KAVin0Sm1kKGCUg\nmzW9/G53TcTsMFPMGUMcZ+h7+ijGRJM5B+b65qUw/L3aDcW9qkXKNCXX1kULmpLk\nbzi7frDb8r/7m9KxCy6cLtxeeZ5CD1oOc+NTqcjcFxk2JNxYdPq5FZAFZyAkk1fr\nXhe5+pc5zc2njCxExF56kU9N\n-----END PRIVATE KEY-----\n",
+  "client_email": "oauth-service-ashton-jones-dev@midyear-pattern-286419.iam.gserviceaccount.com",
+  "client_id": "104665520010538800871",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/oauth-service-ashton-jones-dev%40ashton-jones-dev-website.iam.gserviceaccount.com"
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/oauth-service-ashton-jones-dev%40midyear-pattern-286419.iam.gserviceaccount.com"
 }
-''');
+''', impersonatedUser: 'ashton@grapeworks.dev');
 
   GmailApi _gmailApi;
 
@@ -48,11 +48,14 @@ class _ConsultingTabState extends State<ConsultingTab> {
   void initState() {
     super.initState();
 
-    print('CREDENTIALS: Client ID: ${_credentials.clientId} | Client Secret: ${_credentials.privateKey}');
+    print('CREDENTIALS: Client ID: ${_credentials.clientId.toString()} | Client Secret: ${_credentials.privateKey}');
 
     // Authenticate the credentials with the service account and use them to initialize the GmailAPI
     clientViaServiceAccount(_credentials, scopes).then((http_client) {
       _gmailApi = GmailApi(http_client);
+
+      print('Users: ${_gmailApi.users}');
+
     });
 
 
@@ -143,18 +146,8 @@ class _ConsultingTabState extends State<ConsultingTab> {
 
     print(mimeMessage);
 
-    var mimeMessageBuilder = MessageBuilder(text: 'hello there');
-
-    mimeMessage.to = [MailAddress('Ashton Jones', 'guitarman76940@gmail.com')];
-
-    mimeMessageBuilder.from = [MailAddress('Ashton Jones', 'ashtonjonesdev@gmail.com')];
-
-    mimeMessageBuilder.subject = "cool subject";
-
-    MimeMessage mimeMessage2 = mimeMessageBuilder.buildMimeMessage();
-
     // Encode the MimeMessage to base64String
-    Uint8List asciiCodec = AsciiCodec().encode(mimeMessage2.bodyRaw);
+    Uint8List asciiCodec = AsciiCodec().encode(mimeMessage.bodyRaw);
 
     String encodedMessage = base64Encode(asciiCodec);
 
@@ -171,10 +164,15 @@ class _ConsultingTabState extends State<ConsultingTab> {
     print('Encoded message: ${message.raw}');
 
     //
-
-    Message messageCompleted = await _gmailApi.users.messages.send(message, 'ashtonjonesdev@gmail.com').catchError(( error) {
+//
+    Message messageCompleted = await _gmailApi.users.messages.send(message, 'ashton@grapeworks.dev').catchError(( error) {
       print(error);
     });
+
+    print(messageCompleted);
+
+
+
 
 
    }
